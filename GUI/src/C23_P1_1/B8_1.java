92027e4fd2173e6b3c433babba47205bbe54f2f2
Lâm Đức Kính <89448648+KinhLam@users.noreply.github.com>
2022-09-16 11:30:22 +0700

    update java 16/9/2022

---
+++ GUI/src/C23_P1_1/B8_1.java
@@ -0,0 +1,21 @@
+package C23_P1_1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LHUser
+ */
+public class B8_1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int tongchan = 0;
+        for (int i = 0; i < 10; i++) {
+            tongchan +=i*2;
+        }
+        System.out.println("Tổng 10 số chẵn đầu tiên là:" + tongchan);
+    }
+}
---
+++ GUI/src/C23_P1_1/B8_6.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package C23_P1_1;
+
+/**
+ *
+ * @author LHUser
+ */
+public class B8_6 {
+    
+}
