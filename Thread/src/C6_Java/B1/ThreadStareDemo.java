2ff5f1931427cb595f30d15b8aa80ac919b343d9
Lâm Đức Kính <89448648+KinhLam@users.noreply.github.com>
2022-10-07 10:55:36 +0700

    updata

---
+++ Thread/src/C6_Java/B1/ThreadStareDemo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package C6_Java.B1;
+
+/**
+ *
+ * @author Akalis
+ */
+public class ThreadStareDemo extends Thread{
+    static Thread th;
+
+    public static void main(String[] args) {
+        th = new ThreadStareDemo();
+        System.out.println("Thread th is born");
+        System.out.println("Thread th is ready");
+        th.start();
+    }
+    public void run(){
+        System.out.println("Thread th is running");
+    }
+}
